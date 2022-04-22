1c72e804dd9bb222ae9a4540cd98a4498b5f8878
ghostlightning <101944449+ghostlightning@users.noreply.github.com>
2022-04-22 18:50:02 -0300

    Update JogJava.java

--- jog.java/src/jog/java/JogJava.java
+++ jog.java/src/jog/java/JogJava.java
@@ -23,21 +23,26 @@ public class JogJava {
               show.println("\nestava pensando aqui e eu queria perguntar se quer participar de um jogo?\n1->S\n2->N");
               opcao=scan.nextInt();
               
-              if(opcao==2){
+              if(opcao==2) {
                   show.println("deixa pra outra vez entao.");
-                  System.exit(0);}
+                  System.exit(0);
+              }
               else if(opcao==1){
         show.println("Que bom que voce quer jogar,vou explicar como funciona.");
-    }
+               }
               show.println("O jogo e o seguinte,vou pensar em um número de 0 a 10, e voce vai ter que adivihar qual numero e.");
               show.println("\n Esta se perguntando o que vai ganhar com isso?\n1->S\n2->N");
               opcao=scan.nextInt();
+        
               if(opcao==1){show.println("Com o jogo voce ira ganhar um incrivel nada, bom, se voce gostar da sensacao de ganhar alguma coisa vai ser esse o seu ganho.");
-              }else if(opcao==2){show.println("Nao?ok...");}
-              show.println("Bom, agora que voce sabe mais ou menos como vai ser o jogo vou escolher um numero");
-              escolha_computador=gerador.nextInt(11);
-              show.println("pronto ja escolhi um numero "+nome+".");
-              show.println("tente adivinhar agora sabichao:");
+              }
+                else if(opcao==2){
+                    show.println("Nao?ok..."); 
+                }
+                    show.println("Bom, agora que voce sabe mais ou menos como vai ser o jogo vou escolher um numero");
+                    escolha_computador=gerador.nextInt(11);
+                    show.println("pronto ja escolhi um numero "+nome+".");
+                    show.println("tente adivinhar agora sabichao:");
               
               do{ escolha_usuario=scan.nextInt();
               if(escolha_usuario!=escolha_computador){
